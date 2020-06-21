58764ae65514cb4db8e5ff302a7d4f5717ff885f
iceboy07 <isswarya25@gmail.com>
2020-06-22 05:04:17 +0530

    about javascript string challenge

---
+++ stringchallenge.js
@@ -0,0 +1,11 @@
+
+
+alert("Welcome user to this page!!");
+
+var name = prompt("What is your name?");
+
+var favColour = prompt("what is your favourite colour?");
+
+document.write("Hello "+name+" Your Favourite Colour is "+favColour);
+
+console.log("Program Completed");
