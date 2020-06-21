5afc2ad4d9c0390b744690039cb9f24a99366189
iceboy07 <isswarya25@gmail.com>
2020-06-22 04:39:06 +0530

    about javascript string from user

---
+++ stringinjs.js
@@ -0,0 +1,11 @@
+
+
+var name = prompt("What is your name?");
+
+var hotlink = "click here";
+
+var url = "http://www.google.com";
+
+
+document.write("Hello " +name);
+document.write("Please "+hotlink.link(url)+" for Google website");
