033fe5104b80536341d42a5b462217baa9a316cc
iceboy07 <isswarya25@gmail.com>
2020-06-22 03:50:01 +0530

    about javascript console

---
+++ js.js
@@ -0,0 +1,9 @@
+
+alert("Let's Begin The Challenge!");
+
+document.write("Congrats You Fixed This Line Of Code!");
+
+
+console.log();("Good Job Your Almost There!");
+
+console.log("If this appears in the console then you have completed the challenge, Well Done!");
